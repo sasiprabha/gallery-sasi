c2ba3dee6895c599eb045db057202173d66dba5d
SasiPrabha <sasiprabha008@gmail.com>
2012-04-17 17:20:53 +0530

    Initial Commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PhoneGallery::Application.config.secret_token = 'f2b607e178d1f8a7d9ef302d1fa630a11a9ba2719dddc7a3c67c93b96fcadac712b66ff25675cf11d51f3cace461adc275c8ccdf32a347356ffc02e5d4c95d4a'
+PhoneGallery::Application.config.secret_token = '08335b6196fe5cb2d928c91db60d0c7f20ed887de8cdf77524f5db76491db30bebcb4024a01e58aa88b31df1e3deab488ddbc4c6c70020921b413d1f7ac5c3d7'
